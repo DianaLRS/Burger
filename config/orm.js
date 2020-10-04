94e4aee718ad856b426fdfa8f69f3edfe0109715
Luna <dlramos194@gmail.com>
2020-10-04 17:16:13 -0500

    handlebars,orm,connection,controller, schema initial layout set

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,14 @@
 var connection = require("../config/connection.js");
+
+selectAll()
+
+
+
+insertOne()
+
+
+
+updateOne()
+
+// Export connection
+module.exports = orm;
--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,9 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers(
-    id AUTO_INCREMENT,
+    id int NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(100) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id) 
 );
+
